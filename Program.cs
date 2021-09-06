c33c0763d446056f8894a007a952758ee739946e
vlad <39220694+vlayerrt@users.noreply.github.com>
2021-09-06 12:49:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, result;
+            try
+            {
+                Console.Write("введите первое число: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("введите второе число: ");
+                b = Convert.ToDouble( Console.ReadLine());
+                if (b == 0) throw new DivideByZeroException();
+                Console.WriteLine("результат: " + (result = a / b));
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("деление на 0 не предусмотрено");
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("введено неверное значение");
+            }
+            catch 
+            {
+                Console.WriteLine("неизвестная ошибка операции");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("размер типа данных int равен " + sizeof(int));
+            Console.WriteLine("размер типа данных float равен " + sizeof(float));
+            Console.ReadKey();
+            }
+    }
+}
