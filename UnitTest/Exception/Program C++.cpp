28b8a13a47537b5f8de9f7639093451bb8109bd3
vlad <39220694+vlayerrt@users.noreply.github.com>
2021-09-17 22:44:40 +0300

    Update and rename exceptioner.cpp to Program C++.cpp

--- UnitTest/Exception/Program C++.cpp
+++ UnitTest/Exception/Program C++.cpp
@@ -1,5 +1,4 @@
-﻿// exceptioner.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+
 
 #include <iostream>
 using namespace std;
@@ -16,20 +15,18 @@ int main()
         cin >> a;
         cout << "введите второе число: ";
         cin >> b;
-        if (b == 0 ) {
+        if (b == 0) {
             throw "деление на 0 не предусмотрено\n";
-            /*cout << b;*/
         }
-        else throw a,b;
+        else throw a, b;
     }
     catch (const char* exception)
     {
-        cerr << "Error: " << exception << '\n';
+        cerr << "Error: " << exception << endl;
     }
     catch (double dbeer)
     {
-        //cerr << "Error: " << dbeer << '\n';
-        cerr << "\nвведено неверное значение"<<endl;
+        cerr << "\nвведено неверное значение" << endl;
     }
     catch (exception& e)
     {
@@ -39,8 +36,7 @@ int main()
     {
         cerr << "ошибка операции 0" << endl;
     }
-
-    cout << "размер типа данных int = " << sizeof(int) << " bytes"<< endl;
+    cout << "размер типа данных int = " << sizeof(int) << " bytes" << endl;
     cout << "размер типа данных float = " << sizeof(float) << " bytes" << endl;
     cout << "размер типа данных char = " << sizeof(char) << " bytes" << endl;
 
